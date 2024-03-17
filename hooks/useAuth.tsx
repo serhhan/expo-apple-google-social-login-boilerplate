2f6eafc1ef5efd72c316146e1333d822e4a9780c
Serhan Elmali <serhanelmali@gmail.com>
2024-03-18 01:10:59 +0300

    feat: Add secure storage for user data

--- hooks/useAuth.tsx
+++ hooks/useAuth.tsx
@@ -3,9 +3,9 @@ import React, { useState, useEffect, createContext, useContext } from "react";
 import * as WebBrowser from "expo-web-browser";
 import * as Google from "expo-auth-session/providers/google";
 import * as AppleAuthentication from "expo-apple-authentication";
-import AsyncStorage from "@react-native-async-storage/async-storage";
 import { AuthSessionResult } from "expo-auth-session/build/AuthSession.types";
 import { AuthRequestPromptOptions } from "expo-auth-session/build/AuthRequest.types";
+import * as SecureStore from "expo-secure-store";
 
 interface AuthContextType {
   userInfo: any; // You can replace 'any' with a more specific type tailored to your user info structure
@@ -23,6 +23,7 @@ const AuthContext = createContext<AuthContextType | null>(null);
 export const AuthProvider = ({ children }: { children: React.ReactNode }) => {
   const [userInfo, setUserInfo] = useState(null);
   const [appleAuthAvailable, setAppleAuthAvailable] = useState(false);
+  const [appleUserToken, setAppleUserToken] = useState(null);
   const [request, response, promptAsyncGoogle] = Google.useAuthRequest({
     iosClientId: process.env.EXPO_PUBLIC_IOS_CLIENT_ID,
     webClientId: process.env.EXPO_PUBLIC_WEB_CLIENT_ID,
@@ -37,6 +38,13 @@ export const AuthProvider = ({ children }: { children: React.ReactNode }) => {
     const checkAvailable = async () => {
       const isAvailable = await AppleAuthentication.isAvailableAsync();
       setAppleAuthAvailable(isAvailable);
+
+      if (isAvailable) {
+        const user = await SecureStore.getItemAsync("@appleUser");
+        if (user) {
+          setUserInfo(JSON.parse(user));
+        }
+      }
     };
     checkAvailable();
   }, []);
@@ -52,14 +60,14 @@ export const AuthProvider = ({ children }: { children: React.ReactNode }) => {
       );
       const user = await response.json();
       setUserInfo(user);
-      await AsyncStorage.setItem("@user", JSON.stringify(user));
+      await SecureStore.setItemAsync("@googleUser", JSON.stringify(user));
     } catch (error) {
       console.error(error);
     }
   };
 
   async function handleSignInWithGoogle() {
-    const user = await AsyncStorage.getItem("@user");
+    const user = await SecureStore.getItemAsync("@googleUser");
     if (!user) {
       if (response?.type === "success") {
         await getUserInfoGoogle(response.authentication!.accessToken);
@@ -77,8 +85,7 @@ export const AuthProvider = ({ children }: { children: React.ReactNode }) => {
           AppleAuthentication.AppleAuthenticationScope.EMAIL,
         ],
       });
-      // Here, you should handle credentials received.
-      console.log(credential);
+      SecureStore.setItemAsync("@appleUser", JSON.stringify(credential.user));
     } catch (e) {
       console.error(e);
     }
