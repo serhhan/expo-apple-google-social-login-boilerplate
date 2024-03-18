e118147ba8062a154829cb1b1a4fa61fd13effa1
Serhan Elmali <serhanelmali@gmail.com>
2024-03-18 14:45:30 +0300

    style(app): update button styles

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -2,8 +2,9 @@ import { Button, StatusBar, StyleSheet } from "react-native";
 
 import { Text, View } from "@/components/Themed";
 import { useAuth } from "@/hooks/useAuth";
+import { FontAwesome5 } from "@expo/vector-icons";
 
-export default function TabOneScreen() {
+export default function LoginScreen() {
   const {
     userInfo,
     promptAsyncGoogle: googleLogin,
@@ -20,9 +21,23 @@ export default function TabOneScreen() {
         </>
       )}
       {appleAuthAvailable && (
-        <Button title="Sign in with Apple" onPress={() => appleLogin()} />
+        <FontAwesome5.Button
+          name="apple"
+          onPress={() => appleLogin()}
+          style={styles.appleButton}
+          iconStyle={{ fontSize: 24 }}
+        >
+          <Text style={styles.appleButtonText}>Continue with Apple</Text>
+        </FontAwesome5.Button>
       )}
-      <Button title="Sign in with Google" onPress={() => googleLogin()} />
+      <FontAwesome5.Button
+        name="google"
+        onPress={() => googleLogin()}
+        iconStyle={{ color: "black" }}
+        style={styles.googleButton}
+      >
+        <Text>Continue with Google</Text>
+      </FontAwesome5.Button>
     </View>
   );
 }
@@ -37,9 +52,24 @@ const styles = StyleSheet.create({
     fontSize: 20,
     fontWeight: "bold",
   },
-  separator: {
-    marginVertical: 30,
-    height: 1,
-    width: "80%",
+  googleButton: {
+    display: "flex",
+    justifyContent: "center",
+    width: 200,
+    height: 44,
+    backgroundColor: "white",
+    borderColor: "black",
+    borderWidth: 1,
+    borderRadius: 5,
+  },
+  appleButton: {
+    display: "flex",
+    justifyContent: "center",
+    width: 200,
+    height: 44,
+    backgroundColor: "black",
+  },
+  appleButtonText: {
+    color: "white",
   },
 });
