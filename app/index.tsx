8de402e4c4a1b8210463d82902fcfd8d2bdc4756
Serhan Elmali <serhanelmali@gmail.com>
2024-03-18 15:52:42 +0300

    feat: Update app index to display home page with link to login page

--- app/index.tsx
+++ app/index.tsx
@@ -1,5 +1,11 @@
-import LoginScreen from "@/screens/LoginScreen";
+import { Text, View } from "@/components/Themed";
+import { Link } from "expo-router";
 
-export default function LoginPage() {
-  return <LoginScreen />;
+export default function HomePage() {
+  return (
+    <View>
+      <Text>Home page</Text>
+      <Link href="/login">Login page</Link>
+    </View>
+  );
 }
